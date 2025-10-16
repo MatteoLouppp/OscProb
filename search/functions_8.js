9ec48bafa2ae7b97e84861a1de9413a09133fedb
MatteoLouppp <MatteoLouppp@users.noreply.github.com>
2025-10-16 16:39:01 +0000

    deploy: 92ceb71e58b1fbd834a5c6b202ee3c37ef86be76

--- search/functions_8.js
+++ search/functions_8.js
@@ -1,8 +1,7 @@
 var searchData=
 [
   ['idxcompare_0',['IdxCompare',['../structOscProb_1_1IdxCompare.html#acf7d8627e8dfa6bfc821896647f3d7e8',1,'OscProb::IdxCompare']]],
-  ['initializeprem_1',['InitializePrem',['../classOscProb_1_1PMNS__TaylorExp.html#ad9c9e32d61b8d2847a82ad9b2b7e558d',1,'OscProb::PMNS_TaylorExp']]],
-  ['initializetaylorsvectors_2',['InitializeTaylorsVectors',['../classOscProb_1_1PMNS__TaylorExp.html#a7f4323b6137760dd931868413c0fe61d',1,'OscProb::PMNS_TaylorExp']]],
-  ['initializevectors_3',['InitializeVectors',['../classOscProb_1_1PMNS__Base.html#adf23b569112f9f9e0e592f01d79a5f3d',1,'OscProb::PMNS_Base']]],
-  ['initmatrix_4',['InitMatrix',['../classOscProb_1_1PMNS__NUNM.html#ab53487890c4b2b98b099663317dbf2b7',1,'OscProb::PMNS_NUNM']]]
+  ['initializetaylorsvectors_1',['InitializeTaylorsVectors',['../classOscProb_1_1PMNS__TaylorExp.html#a7f4323b6137760dd931868413c0fe61d',1,'OscProb::PMNS_TaylorExp']]],
+  ['initializevectors_2',['InitializeVectors',['../classOscProb_1_1PMNS__Base.html#adf23b569112f9f9e0e592f01d79a5f3d',1,'OscProb::PMNS_Base']]],
+  ['initmatrix_3',['InitMatrix',['../classOscProb_1_1PMNS__NUNM.html#ab53487890c4b2b98b099663317dbf2b7',1,'OscProb::PMNS_NUNM']]]
 ];
